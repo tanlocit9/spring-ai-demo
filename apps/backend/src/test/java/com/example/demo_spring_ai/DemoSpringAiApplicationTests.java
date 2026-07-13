bcb46043fc0a6522d96f15171c1c59e6a8ba4bcf
SodNguyen <tanlocit9@gmail.com>
2026-07-13 20:07:05 +0700

    init

---
+++ apps/backend/src/test/java/com/example/demo_spring_ai/DemoSpringAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_spring_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
